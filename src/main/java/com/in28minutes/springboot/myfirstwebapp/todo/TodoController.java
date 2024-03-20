2deb9b4f0c17ab1345f12fca035b6dd46290fb8c
quang <mnquang123@gmail.com>
2024-03-21 00:02:24 +0700

    Add add-todo page

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 	
 	private TodoService todoService;
@@ -21,5 +24,14 @@ public class TodoController {
 		List<Todo> todos = todoService.findByUsername("in28minutes");
 		model.addAttribute("todos", todos);
 		return "listTodos";
+	}
+
+	@RequestMapping(value="add-todo", method= RequestMethod.GET)
+	public String showNewTodoPage(ModelMap model) {
+		return "todo";
+	}
+	@RequestMapping(value="add-todo", method= RequestMethod.POST)
+	public String addNewTodo() {
+		return "redirect:list-todos";
 	}
 }
