6b09ddef3b2ef92c00b64f8f98e6055fa4614c4e
quang <mnquang123@gmail.com>
2024-03-21 23:47:45 +0700

    Add validation when adding new todo

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class Todo {	
@@ -15,6 +17,8 @@ public class Todo {
 
 	private int id;
 	private String username;
+
+	@Size(min=10, message="Enter at least 10 Characters...")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,8 +3,10 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,11 +32,16 @@ public class TodoController {
 
 	@RequestMapping(value="add-todo", method= RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
+		Todo todo = new Todo(0, (String)model.get("name"), "Default Desc", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
 		return "todo";
 	}
 	@RequestMapping(value="add-todo", method= RequestMethod.POST)
-	public String addNewTodo(@RequestParam String description, ModelMap model) {
-		todoService.addTodo((String)model.get("name"), description, LocalDate.now().plusYears(1), false);
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		todoService.addTodo((String)model.get("name"), todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
